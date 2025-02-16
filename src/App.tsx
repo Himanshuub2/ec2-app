e078999d63e6c779271e521c69f33c82c737a815
iihimanshu <iihimanshu.b2@hotmail.com>
2025-02-16 20:29:58 +0530

    initialize

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import img from './assets/myimage.jpeg'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <img src={img} alt="my image" width={300} height={300} /> 
+      <br/>
+      <h1>Hello All</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
